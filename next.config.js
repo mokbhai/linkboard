82241d48e6ca09c06d0c7b4c557ce56d84b53aa8
mokshit <mokshitjain18@gmail.com>
2025-11-09 12:06:28 +0530

    feat: add peer dependency flag to relevant packages in package-lock.json

--- next.config.js
+++ next.config.js
@@ -1,19 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	compiler: {
-		styledComponents: {
-			pure: true,
-			displayName: true,
-		},
-	},
-	images: {
-		remotePatterns: [
-			{
-				hostname: 'api.qrserver.com',
-				protocol: 'https',
-			}
-		]
-	},
+  output: "export",
+  compiler: {
+    styledComponents: {
+      pure: true,
+      displayName: true,
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "api.qrserver.com",
+        protocol: "https",
+      },
+    ],
+    unoptimized: true, // Required for static export
+  },
 };
 
 module.exports = nextConfig;
